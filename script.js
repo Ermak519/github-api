56600b2665efc7b5d9f66487b13fa99476338223
Vyacheslav <ermak519@gmail.com>
2021-12-11 15:38:36 +0300

    GitHub API deploy

--- script.js
+++ script.js
@@ -2,51 +2,37 @@ const githubList = document.querySelector('.github-api__list');
 const input = document.querySelector('input');
 const dropdownList = document.querySelector('.dropdown__list')
 const dropdownItems = document.querySelectorAll('.dropdown__item')
-dropdownList.addEventListener('change', () => {
-    !input.value ? console.log('dskjf') : console.log('11111111111')
-})
+let arr = []
+
 async function getGithubData(query) {
-    const URL = `https://api.github.com/search/repositories?q=${query}`;
-    const data = await fetch(URL);
+    !query ? dropdownList.classList.remove('show') : dropdownList.classList.add('show');
+
+    const data = await fetch(`https://api.github.com/search/repositories?q=${query}+&sort=stars`);
     const { items } = await data.json();
+    arr = items.slice()
     dropdownItems.forEach((item, i) => {
-        item.textContent = items[i].name
+        item.textContent = items[i].name;
+        item.dataset.idrepo = items[i].id
     })
 }
 
-const debounce = (fn, debounceTime) => {
-    let timeOut;
-    return function () {
-        const wrapper = () => { fn.apply(this, arguments) }
-        clearTimeout(timeOut)
-        timeOut = setTimeout(wrapper, debounceTime)
-    }
-};
-
-function onChange(e) {
-    getGithubData(e.target.value)
-}
-
-onChange = debounce(onChange, 500)
-input.addEventListener('keyup', onChange)
-
-
-const addGithubUser = (...arrUsers) => {
+function githubCard(user) {
+    const { name, owner, stargazers_count } = user
     const listItem = document.createElement('li');
     listItem.classList.add("github-api__item", 'user');
     const htmlNode = `
             <ul class="user__list">
                 <li class="user__item user__name">
                     <p>Name:</p>
-                    <p>${arrUsers[0]}</p>
+                    <p>${name}</p>
                 </li>
                 <li class="user__item user__owner">
                     <p>Owner:</p>
-                    <p>${arrUsers[1]}</p>
+                    <p>${owner.login}</p>
                 </li>
                 <li class="user__item user__stars">
                     <p>Stars:</p>
-                    <p>${arrUsers[2]}</p>
+                    <p>${stargazers_count}</p>
                 </li>
             </ul>
             <div class="btn">
@@ -61,11 +47,35 @@ const addGithubUser = (...arrUsers) => {
     });
 };
 
+function debounce(fn, debounceTime) {
+    let timeOut;
+    return function () {
+        const wrapper = () => { fn.apply(this, arguments) }
+        clearTimeout(timeOut)
+        timeOut = setTimeout(wrapper, debounceTime)
+    }
+};
+
+function onChange(e) {
+    getGithubData(e.target.value)
+}
+
+input.addEventListener('keyup', debounce(onChange, 180));
+
 dropdownItems.forEach((item) => {
     item.addEventListener('click', () => {
-        console.log('click')
-        addGithubUser()
+        dropdownList.classList.remove('show');
+        input.value = '';
+    });
+    item.addEventListener('click', () => {
+        const repo = arr.filter(obj => obj.id == item.dataset.idrepo)
+        githubCard(...repo)
     })
 })
+
+
+
+
+
 
 
