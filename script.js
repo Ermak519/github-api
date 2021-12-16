dd582a38c13e01b81c687f6004f66597b1d90313
Vyacheslav <ermak519@gmail.com>
2021-12-16 22:28:35 +0300

    fix code

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ const githubList = document.querySelector('.github-api__list');
 const input = document.querySelector('input');
 const dropdownList = document.querySelector('.dropdown__list')
 const dropdownItems = document.querySelectorAll('.dropdown__item')
-const arrQuery = []
+let arrQuery = []
 
 const getGithubData = async (query) => {
     if (query) {
         const data = await fetch(`https://api.github.com/search/repositories?q=${query}+&sort=stars`);
         !data ? dropdownList.classList.remove('show') : dropdownList.classList.add('show');
         const { items } = await data.json();
-        items.forEach((obj) => {
-            arrQuery.push(obj)
-        })
+        arrQuery = items.slice()
         dropdownItems.forEach((item, i) => {
             item.textContent = items[i].name;
             item.dataset.idrepo = items[i].id
